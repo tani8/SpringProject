d1825092127a3c383d5297d4f1dbf7d06f19a1fd
Taniya P <taniya23@ad3.ucdavis.edu>
2014-08-12 08:28:14 -0700

    pom fixed

---
+++ SpringProj/src/main/java/SpringProj/web/HomeController.java
@@ -0,0 +1,15 @@
+package com.beingjavaguys.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/hello")
+    public ModelAndView test() {
+        String message = "Welcome to Spring 4.0 !";
+        return new ModelAndView("hello", "message", message);
+    }
+} 
